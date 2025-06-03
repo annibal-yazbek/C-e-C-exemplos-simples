824f4b5c5f95f6b006658e97d446b85e788b033b
Annibal <annibal.yazbek@icloud.com>
2025-06-03 10:51:16 -0300

    Com namespace std

--- olamundo.cpp
+++ olamundo.cpp
@@ -8,12 +8,23 @@ int main(void)
 }
 */
 
-// em C++
+/* em C++
 #include <iostream>
 
 int main(void)
 {
     std::cout << "Ola Mundo C++!" << std::endl;
+    return 0;
+}
+*/
+
+// em C++ com  namespace std
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Ola Mundo C++!" << endl;
     return 0;
 }
 
