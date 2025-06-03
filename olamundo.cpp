19e22b5b68bd88cfa11cca66fbbdfe65f54af9bd
Annibal <annibal.yazbek@icloud.com>
2025-06-03 11:31:51 -0300

    Variaveis em C e C++

--- olamundo.cpp
+++ olamundo.cpp
@@ -20,11 +20,28 @@ int main(void)
 
 // em C++ com  namespace std
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(void)
 {
-    cout << "Ola Mundo C++!" << endl;
+    int numero = 10; // Exemplo de variável 
+    float pi = 3.1415; // Exemplo de variável float
+    double numero_grande = 999999999999999999999999999999999.99999999; //Exemplo de variável double
+    char letra = 'A'; // Exemplo de variável char
+    
+    string nome = "João"; // Exemplo de variável string
+
+    
+    cout << "O número é " << numero << endl;
+    cout << "O valor de PI é " << pi << endl;
+    cout << "O número grande é " << numero_grande << endl;
+    cout << "A letra é " << letra << endl;
+
+    cout << "O nome é " << nome << endl;
+
+
     return 0;
 }
 
