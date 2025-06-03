10a3d999f7784b3a90807177ec5be372cfe701b6
Annibal <annibal.yazbek@icloud.com>
2025-06-03 10:39:19 -0300

    ola mundo c++

--- olamundo.cpp
+++ olamundo.cpp
@@ -1,3 +1,4 @@
+/* em C
 #include <stdio.h>
 
 int main(void)
@@ -5,3 +6,14 @@ int main(void)
     puts("Ola Mundo!");
     return 0;
 }
+*/
+
+// em C++
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Ola Mundo!" << std::endl;
+    return 0;
+}
+
