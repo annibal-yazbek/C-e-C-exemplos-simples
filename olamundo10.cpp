3a978cd51bc7e604c686eeb36c5ce99178043c51
Annibal <annibal.yazbek@icloud.com>
2025-06-06 10:55:02 -0300

    usando do while

---
+++ olamundo10.cpp
@@ -0,0 +1,20 @@
+// usando do while  C++
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int numero = 0;
+
+    do 
+    {
+        cout << numero << endl;
+        numero ++;
+
+    } while( numero < 10 );
+
+    getch();
+    return 0;
+}
