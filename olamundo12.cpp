8d7f8c5ad2b66ce886860d21b55842b6c0324e39
Annibal <annibal.yazbek@icloud.com>
2025-06-09 10:59:34 -0300

    Struct e array - II  em C/ C++

--- olamundo12.cpp
+++ olamundo12.cpp
@@ -11,7 +11,6 @@ struct pessoas
     int pessoa_id;
     string nome;
     int idade;
-
 };
 
 int main() 
@@ -23,11 +22,42 @@ int main()
     pe2 = {2, "Maria", 20},
     pe3 = {3, "José", 30};
 
-//   for( int i = 1; i <= 3; i++)
+//  definindo struct como array
+    pessoas p[10];
+
+
 
     cout << "Id Pessoa 1 : " << pe1.pessoa_id << endl;
     cout << "Nome Pessoa 1 : " << pe1.nome << endl;
     cout << "Idade Pessoa 1 : " << pe1.idade << endl;
+
+    cout << endl << endl;
+
+
+//  loop para entrar struct em array com 3 elementos
+
+    for( int i = 0; i < 3; i++)
+    {
+        cout << "Entre com o id da pessoa " << i + 1 << ": " ;
+        cin >> ( p[i].pessoa_id );
+        cout << "Entre com o nome da pessoa " << i + 1 << ": " ;
+        cin >> ( p[i].nome );
+        cout << "Entre com a idade da pessoa " << i + 1 << ": " ;
+        cin >> ( p[i].idade );
+        cout << endl;
+
+    }
+
+    //mostrando o array de structs
+
+    for( int i = 0; i < 3; i++)
+    {
+        cout << "Id da pessoa " << i + 1 << ": " << p[i].pessoa_id << endl;
+        cout << "Nome da pessoa " << i + 1 << ": " << p[i].nome << endl;
+        cout << "Idade da pessoa " << i + 1 << ": " << p[i].idade << endl;
+        cout << endl;
+
+    }
 
     getch();
 
